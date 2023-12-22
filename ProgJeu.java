8966eb7b821d9ae7294d8f1d9652f274b9eafc11
Rémi <remi.choffat@gmail.com>
2023-12-22 12:39:45 +0100

    Lancer nouvelle partie

--- ProgJeu.java
+++ ProgJeu.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
+/**
+ * Lance les parties du jeu
+ */
 class ProgJeu {
 
   /**
@@ -5,9 +10,16 @@ class ProgJeu {
    */
   public static void main(String [] args) {
 
-    // Crée un jeu avec 100 cartes
-    Jeu jeu = new Jeu(100);
-    jeu.lancerJeu();
+    Scanner sc = new Scanner(System.in);
+    // Lance une nouvelle partie à la fin de chaque jeu
+    while (true){
+      // Crée un jeu avec 100 cartes
+      Jeu jeu = new Jeu(100);
+      jeu.lancerJeu();
+      // Permet de lancer une nouvelle partie
+      System.out.println("Tapez Entrée pour commencer une nouvelle partie ");
+      sc.nextLine();
+    }
 
   }
 }
